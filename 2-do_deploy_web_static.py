653552ae473e322db10de929ec166d50dae0a7bf
alx-system_engineering_devops <89623556+DevepProd@users.noreply.github.com>
2022-09-30 01:40:17 +0200

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,47 +1,37 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
 """
-Created on Mon Aug 13 14:21:54 2020
-@author: Robinson Montes
+script (based on the file 1-pack_web_static.py) that distributes
+an archive to your web servers
 """
-from fabric.api import local, put, run, env
-from datetime import datetime
 
-env.user = 'ubuntu'
-env.hosts = ['35.227.35.75', '100.24.37.33']
+from fabric.api import run, put, env
+import os
 
-
-def do_pack():
-    """
-    Targginng project directory into a packages as .tgz
-    """
-    now = datetime.now().strftime("%Y%m%d%H%M%S")
-    local('sudo mkdir -p ./versions')
-    path = './versions/web_static_{}'.format(now)
-    local('sudo tar -czvf {}.tgz web_static'.format(path))
-    name = '{}.tgz'.format(path)
-    if name:
-        return name
-    else:
-        return None
+env.hosts = ['34.138.245.164', '54.224.201.40']
 
 
 def do_deploy(archive_path):
-    """Deploy the boxing package tgz file
     """
+    distributes an archive to your web servers
+    """
+    # verificamos si el path existe
+    if os.path.exists(archive_path) is False:
+        return(False)
     try:
-        archive = archive_path.split('/')[-1]
-        path = '/data/web_static/releases/' + archive.strip('.tgz')
-        current = '/data/web_static/current'
-        put(archive_path, '/tmp')
-        run('mkdir -p {}/'.format(path))
-        run('tar -xzf /tmp/{} -C {}'.format(archive, path))
-        run('rm /tmp/{}'.format(archive))
-        run('mv {}/web_static/* {}'.format(path, path))
-        run('rm -rf {}/web_static'.format(path))
-        run('rm -rf {}'.format(current))
-        run('ln -s {} {}'.format(path, current))
-        print('New version deployed!')
-        return True
+        put(archive_path, '/tmp/')
+        _filename = archive_path.split("/")[-1]
+        filename = _filename.split(".")[0]
+        run('mkdir -p /data/web_static/releases/{}'.format(filename))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format
+            (_filename, filename))
+        run('rm /tmp/{}'.format(_filename))
+        run('mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/'.format(filename, filename))
+        run('rm -rf /data/web_static/releases/{}/web_static'
+            .format(filename))
+        run('rm -rf /data/web_static/current')
+        run('ln -s /data/web_static/releases/{} /data/web_static/current'
+            .format(filename))
+        return(True)
     except:
-        return False
+        return(False)
